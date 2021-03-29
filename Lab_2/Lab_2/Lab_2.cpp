b8eea09de4fc9f8f43f2b3720332d92f390f81f2
Dmytro Hushchin <dhuchchin@gmail.com>
2021-03-29 21:23:42 +0300

    Bug in algorithm

--- Lab_2/Lab_2/Lab_2.cpp
+++ Lab_2/Lab_2/Lab_2.cpp
@@ -1,101 +1,151 @@
 ﻿#include <iostream>
 #include <fstream>
 #include <string>
-
+#include <vector>
+#include <iomanip>
+#define INF INT_MAX
 using namespace std;
 
-int** ReadFile(int& rows, int& columns);
-void FindSize(ifstream&, int&, int&);
-int** CreateMatrix(int&, int&);
-void FillMatrix(ifstream&, int**, int&, int&);
+int** ReadFile(int& size);
+void FindSize(int& size);
+int** CreateMatrix(int&);
+void FillMatrix(int**, int&);
 void DeleteMatrix(int**, int&);
-void PrintMatrix(int** Matrix, int& rows, int& columns);
-void Output(int** Matrix, int rows, int columns);
+void PrintMatrix(int** Matrix, int& size);
+void Output(int** Matrix, int size);
+void Floyd_Warshall(int** Matrix, int size);
+int min_value(int a, int b);
+
+vector<string> delim(string str, char delim) {
+    int start, end = 0;
+    vector<string> vect;
+    while ((start = str.find_first_not_of(delim, end)) != string::npos)
+    {
+        end = str.find(delim, start);
+        int length = end - start;
+        if (length == 0) continue;
+
+        string word = string(str, start, length);
+        vect.push_back(word);
+    }
+    return vect;
+}
+
 
 int main()
 {
-    int rows, columns;
-    int** Matrix = ReadFile(rows, columns);
-    Output(Matrix, rows, columns);
-    DeleteMatrix(Matrix, rows);
+    int size;
+    int** Matrix = ReadFile(size);
+    Floyd_Warshall(Matrix, size);
+    PrintMatrix(Matrix, size);
+    Output(Matrix, size);
+    DeleteMatrix(Matrix, size);
 }
 
 
-int** ReadFile(int& rows, int& columns) {
-    ifstream input("..\\iofiles\\input.txt");
-    FindSize(input, rows, columns);
-    int** Matrix = CreateMatrix(rows, columns);
-    input.close();
-    input.clear();
-    input.open("..\\iofiles\\input.txt");
-    FillMatrix(input, Matrix, rows, columns);
-    PrintMatrix(Matrix, rows, columns);
-    input.close();
+int** ReadFile(int& size) {
+    FindSize(size);
+    int** Matrix = CreateMatrix(size);
+    cout << "Reading file..." << endl << endl;
+    FillMatrix(Matrix, size);
+    PrintMatrix(Matrix, size);
     return Matrix;
 }
 
-void FindSize(ifstream& input, int& rows, int& columns) {
+void FindSize(int& size) {
+    ifstream input("..\\iofiles\\input.txt");
     string currStr;
-    rows = 0;
+    size = -1;
     while (!input.eof()) {
         getline(input, currStr);
-        columns = currStr.length();
-        rows++;
+        size++;
+        cout << size << endl;
     }
+
+    input.close();
+    input.clear();
 }
 
-int** CreateMatrix(int& rows, int& columns) {
-    int** Matrix = new int* [rows];
-    for (int i = 0; i < rows; i++)
+int** CreateMatrix(int& size) {
+    int** Matrix = new int* [size];
+    for (int i = 0; i < size; i++)
     {
-        Matrix[i] = new int[columns];
+        Matrix[i] = new int[size];
     }
     return Matrix;
 }
 
-void PrintMatrix(int** Matrix, int& rows, int& columns) {
-    cout << "Inputted this problem:" << endl << endl;
-    for (int i = 0; i < rows; i++)
+void PrintMatrix(int** Matrix, int& size) {
+    cout << "Weight matrix:" << endl;
+    for (int i = 0; i < size; i++)
     {
-        for (int j = 0; j < columns; j++)
+        for (int j = 0; j < size; j++)
         {
-            cout << Matrix[i][j];
+            cout << setw(11) << Matrix[i][j] << ' ';
         }
         cout << endl;
     }
+    cout << endl;
 }
 
-void FillMatrix(ifstream& input, int** Matrix, int& rows, int& columns) {
-    for (int i = 0; i < rows; i++)
+void FillMatrix(int** Matrix, int& size) {
+    ifstream input("..\\iofiles\\input.txt");
+    
+    for (int i = 0; i < size; i++)
     {
         string currStr;
         getline(input, currStr);
-        for (int j = 0; j < columns; j++)
+        vector<string> tmp = delim(currStr, ' ');
+        for (int j = 0; j < size; j++)
         {
-            Matrix[i][j] = currStr[j] - '0';
+            if (tmp[j] == "INF") { 
+                //char ch = tmp[j][0];
+                Matrix[i][j] = INF; 
+            }
+            else {
+                Matrix[i][j] = tmp[j][0] - '0';
+            }
         }
     }
+    input.close();
+    input.clear();
 }
 
-void DeleteMatrix(int** Matrix, int& rows) {
-    for (int i = 0; i < rows; i++)
+void DeleteMatrix(int** Matrix, int& size) {
+    for (int i = 0; i < size; i++)
     {
         delete[] Matrix[i];
     }
     delete Matrix;
 }
 
-void Output(int** Matrix, int rows, int columns)
+void Output(int** Matrix, int size)
 {
     ofstream output("..\\iofiles\\output.txt");
-    for (int i = 0; i < rows; i++)
+    for (int i = 0; i < size; i++)
     {
-        for (int j = 0; j < columns; j++)
+        for (int j = 0; j < size; j++)
         {
-            output << Matrix[i][j];
+            output << Matrix[i][j] << ' ';
         }
         output << endl;
     }
     output.close();
     cout << "Saved to output.txt" << endl;
+}
+
+void Floyd_Warshall(int** matrix, int size) {
+    for (int k = 0; k < size; k++) {	//Run through all vertexes and find the shoutest route throgh vertex k
+        for (int i = 0; i < size; i++) {
+            for (int j = 0; j < size; j++) {
+                matrix[i][j] = min_value(matrix[i][j], matrix[i][k] + matrix[k][j]);	// New weight is minimal between new sum of edges / old one
+            }
+        }
+    }
+}
+
+int min_value(int a, int b) {
+    int min;
+    a < b ? min = a : min = b;
+    return min;
 }
