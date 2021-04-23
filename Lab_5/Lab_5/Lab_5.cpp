0774011565274b55b880b37bdce822499afd9ec3
Dmytro Hushchin <dhuchchin@gmail.com>
2021-04-23 23:23:40 +0300

    Final result

--- Lab_5/Lab_5/Lab_5.cpp
+++ Lab_5/Lab_5/Lab_5.cpp
@@ -1,86 +1,81 @@
 ﻿#include <iostream>
+#include <vector>
 
 using namespace std;
 
 struct Node {
     int data;
-    Node* left, *right;
+    Node* left;
+    Node* right;
 };
 
-class BinaryTree {
-private:
-    Node* root;
-public:
-    BinaryTree(int data);
-    ~BinaryTree();
-    void DeleteTree(Node* leaf);
-    void ShowTree(Node* node);
-    void insert(int data, Node* leaf);
-    Node* GetRoot();
-};
+Node* GetNewNode(int data);
+Node* Insert(Node* root, int data);
+void printLevel(Node* node, int level, vector<int>& vect);
+Node* FillTree(Node* root);
+void GetLevel(Node* root);
 
-int main()
-{
+int main() {
+    Node* root = NULL;
+    root = FillTree(root);
+    GetLevel(root);
+    system("pause");
+    return 0;
+}
+
+
+Node* GetNewNode(int data) {
+    Node* newNode = new Node();
+    newNode->data = data;
+    newNode->left = newNode->right = NULL;
+    return newNode;
+}
+
+Node* Insert(Node* root, int data) {
+    if (root == NULL) {
+        root = GetNewNode(data);
+    }
+    else if (data <= root->data) {
+        root->left = Insert(root->left, data);
+    }
+    else {
+        root->right = Insert(root->right, data);
+    }
+    return root;
+}
+
+void printLevel(Node* node, int level, vector<int>& vect) {
+    if (node == nullptr) {
+        return;
+    }
+    else {
+        printLevel(node->left, level - 1, vect);
+        if (level == 0) {
+            vect.push_back(node->data);
+        }
+        printLevel(node->right, level - 1, vect);
+    }
+}
+
+Node* FillTree(Node* root) {
     int count;
     cout << "Enter the amount of elements you want to enter: "; cin >> count;
-    int elem;
-    cout << "Enter a number: ";
-    cin >> elem;
-    BinaryTree Tree(elem);
-    for (int i = 0; i < count - 1; i++) {
+    for (int i = 0; i < count; i++) {
         int elem;
-        cout << "Enter a number: "; 
+        cout << "Enter a number: ";
         cin >> elem;
-        Tree.insert(elem, Tree.GetRoot());
+        root = Insert(root, elem);
     }
-}
-
-
-BinaryTree::BinaryTree(int data) {
-    root = new Node;
-    root->data = data;
-    root->left = root->right = NULL;
-}
-
-BinaryTree::~BinaryTree() {
-    DeleteTree(root);
-};
-
-void BinaryTree::DeleteTree(Node* node) {
-    if (node != NULL)
-    {
-        DeleteTree(node->left);
-        DeleteTree(node->right);
-        delete node;
-    }
-}
-
-
-void BinaryTree::insert(int data, Node* leaf)
-{
-    if (leaf == NULL)
-    {
-        leaf = new Node;
-        leaf->data = data;
-        leaf->left = leaf->right = NULL;
-    }
-    else if (data < leaf->data)
-        insert(data, leaf->left);
-    else
-        insert(data, leaf->right);
-    cout << leaf->data << endl;
-}
-
-
-void BinaryTree::ShowTree(Node* node)
-{
-        if (node == NULL) return;
-        ShowTree(node->left);
-        cout << node->data << " ";
-        ShowTree(node->right);
-}
-
-
-Node* BinaryTree::GetRoot() {
     return root;
+}
+
+void GetLevel(Node* root) {
+    int level;
+    cout << "Enter level: "; cin >> level;
+    vector<int> vect;
+    printLevel(root, level, vect);
+    cout << "There are " << vect.size() << " elements on the " << level << " level: ";
+    for (int i = 0; i < vect.size(); i++)
+        cout << vect[i] << "  ";
+    cout << endl;
 }
